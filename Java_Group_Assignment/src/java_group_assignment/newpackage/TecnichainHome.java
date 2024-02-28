55294f948df0043b16ade81c563f5c3294baa15f
maan0404 <jokesonyou69691@gmail.com>
2024-02-28 14:13:21 +0800

    small chage

--- Java_Group_Assignment/src/java_group_assignment/newpackage/TecnichainHome.java
+++ Java_Group_Assignment/src/java_group_assignment/newpackage/TecnichainHome.java
@@ -16,7 +16,7 @@ public class TecnichainHome extends javax.swing.JFrame {
     public TecnichainHome() {
         initComponents();
     }
-
+    //ihiygufywytfty
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
